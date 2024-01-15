47967c436a89d284b38066a8ffbfb6141a204664
Jegal MinHyuk <104841568+jgmh99@users.noreply.github.com>
2024-01-15 17:48:12 +0900

    푸터 수정

--- components/footer.js
+++ components/footer.js
@@ -8,7 +8,7 @@ export default function Footer(){
                     <div class="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
                         <Link href="/">
                             <div class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-                                <span class="ml-3 text-xl">Next.js 포트폴리오</span>
+                                <span class="ml-3 text-xl">제갈민혁 Front-End 포트폴리오</span>
                             </div>
                         </Link>
                     
